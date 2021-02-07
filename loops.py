a0f0fe8bdbeaa6837145a731c84efb43627d852e
Jeffrey Wainaina <68434262+geniusdev26@users.noreply.github.com>
2021-02-07 12:00:08 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,14 @@
+#Simple code to display how an if statement can be used to increment a value
+
+
+click = False
+
+like = 0
+
+click = True
+
+if click == True:
+    like += 1
+    click = False
+
+print(like)
